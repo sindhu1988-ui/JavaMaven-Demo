583f800f2a9582a601bd57573ea88afdeb4af839
sindhu <sindhu@dgv.com>
2025-01-01 18:47:45 +0530

    Initial commit for JavaMaven-Demo project

---
+++ src/main/java/com/example/javamvn_demo/JavamvnDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.javamvn_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamvnDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavamvnDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javamvn_demo/JavamvnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javamvn_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamvnDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
